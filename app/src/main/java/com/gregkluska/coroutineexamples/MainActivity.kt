1fe0b792821816f7e480ed96df4f3270b1a455c6
Greg Kluska <kluskagreg@gmail.com>
2020-06-18 00:05:21 +0100

    Tutorial complete

--- app/src/main/java/com/gregkluska/coroutineexamples/MainActivity.kt
+++ app/src/main/java/com/gregkluska/coroutineexamples/MainActivity.kt
@@ -2,10 +2,99 @@ package com.gregkluska.coroutineexamples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            setNewText("Clicked")
+
+            fakeApiRequest()
+
+        }
+    }
+
+    //Method 2
+    private fun fakeApiRequest() {
+
+        CoroutineScope(IO).launch {
+            val executionTime = measureTimeMillis {
+
+                val result1: Deferred<String> = async {
+                    println("debug: launching job1: ${Thread.currentThread().name}")
+                    getResult1FromApi()
+                }
+
+                val result2: Deferred<String> = async {
+                    println("debug: launching job2: ${Thread.currentThread().name}")
+                    getResult2FromApi()
+                }
+
+                setTextOnMainThread("Got ${result1.await()}")
+                setTextOnMainThread("Got ${result2.await()}")
+            }
+            println("debug: total elapsed time: $executionTime")
+        }
+
+
+    }
+
+    //Method 1
+    private fun fakeApiRequest2() {
+
+        val startTime = System.currentTimeMillis()
+
+        val parentJob = CoroutineScope(IO).launch {
+
+            val job1 = launch {
+                val time1 = measureTimeMillis {
+                    println("debug: launching job1 in thread: ${Thread.currentThread().name}")
+                    val result1 = getResult1FromApi()
+                    setTextOnMainThread("Got $result1")
+                }
+                println("debug: completed job1 in $time1 ms.")
+            }
+
+            val job2 = launch {
+                val time2 = measureTimeMillis {
+                    println("debug: launching job2 in thread: ${Thread.currentThread().name}")
+                    val result2 = getResult2FromApi()
+                    setTextOnMainThread("Got $result2")
+                }
+                println("debug: completed job2 in $time2 ms.")
+            }
+        }
+
+        parentJob.invokeOnCompletion {
+            println("debug: total elapsed time: ${System.currentTimeMillis() - startTime}")
+        }
+    }
+
+    private fun setNewText(input: String) {
+        val newText = text.text.toString() + "\n$input"
+        text.text = newText
+    }
+
+    private suspend fun setTextOnMainThread(input: String) {
+        withContext(Main) {
+            setNewText(input)
+        }
+    }
+
+    private suspend fun getResult1FromApi(): String {
+        delay(1000)
+        return "Result #1"
+    }
+
+    private suspend fun getResult2FromApi(): String {
+        delay(1700)
+        return "Result #2"
     }
 }
